4ef697f70d04045bf85b97e7cd92b8c420b0629f
tanmaiaccion <tanmai.naik@accionlabs.com>
2018-11-20 14:26:14 +0530

    Initial dis subtree commit

---
+++ build/precache-manifest.ef4806a28cffb070070772a3bfb4b685.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6cf7c6ec7c2d6f670ae9d762604cb0b",
+    "url": "/static/media/fontawesome-webfont.e6cf7c6e.woff2"
+  },
+  {
+    "revision": "367d50abf964ed71c7b0",
+    "url": "/static/css/main.f6c02686.chunk.css"
+  },
+  {
+    "revision": "279702e05976a44fbbafe245f4085c31",
+    "url": "/static/media/devicon.279702e0.svg"
+  },
+  {
+    "revision": "23f1a46bd32fdd6f08c1",
+    "url": "/static/js/1.23f1a46b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0b01fc09af1a67160397ebbd676c7b5a",
+    "url": "/static/media/banner.0b01fc09.jpg"
+  },
+  {
+    "revision": "25a32416abee198dd821b0b17a198a8f",
+    "url": "/static/media/fontawesome-webfont.25a32416.eot"
+  },
+  {
+    "revision": "c8ddf1e5e5bf3682bc7bebf30f394148",
+    "url": "/static/media/fontawesome-webfont.c8ddf1e5.woff"
+  },
+  {
+    "revision": "367d50abf964ed71c7b0",
+    "url": "/static/js/main.367d50ab.chunk.js"
+  },
+  {
+    "revision": "1dc35d25e61d819a9c357074014867ab",
+    "url": "/static/media/fontawesome-webfont.1dc35d25.ttf"
+  },
+  {
+    "revision": "d7c639084f684d66a1bc66855d193ed8",
+    "url": "/static/media/fontawesome-webfont.d7c63908.svg"
+  },
+  {
+    "revision": "478292ee89c6ed097c8d79908ae5b1dd",
+    "url": "/static/media/devicon.478292ee.eot"
+  },
+  {
+    "revision": "fd2d16f23d2fb36bf259b93ae75e0d42",
+    "url": "/static/media/devicon.fd2d16f2.woff"
+  },
+  {
+    "revision": "b6ba8105eea98fe99e74c8b8c844f266",
+    "url": "/static/media/devicon.b6ba8105.ttf"
+  },
+  {
+    "revision": "23f1a46bd32fdd6f08c1",
+    "url": "/static/css/1.b58d6c5d.chunk.css"
+  },
+  {
+    "revision": "f52a36173434e8fd26917edb41cac42a",
+    "url": "/index.html"
+  }
+];
